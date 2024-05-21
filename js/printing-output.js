7405eedbee13f740b76bf033266acfaec62d2cab
augu0053 <augu0053@algonquinlive.com>
2024-05-21 07:55:50 -0400

    Add files via upload

---
+++ js/printing-output.js
@@ -0,0 +1,57 @@
+// alert( "description of alerted_value: " + alerted_value );
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+/* *********** Task 1 *********** */
+alert( "description of alerted_value: " );
+let a = 3;
+let b = 5;
+let c;
+console.log("let a = 3;");
+console.log("let b = 5;");
+console.log("let c;");
+"-----------"
+let value =
+`a + b = ${a+b} 
+a - b = ${a-b} 
+a * b = ${a*b}
+a / b = ${a/b}
+a % b = ${a%b}
+a += b = ${a+=b}
+a -= b = ${a-=b}
+a *= b = ${a*=b}
+a /= b = ${a/=b}
+a %= b = ${a%=b}
+a == b = ${a==b}
+a != b = ${a!=b}
+a > b = ${a>b}
+a < b = ${a<b}
+!a && !c = ${!a&&!b}
+!a || !c = ${!a||!b}`;
+
+console.log(value);
+
+/* *********** Task 2 *********** */
+
+let first_name = "Adarsh";
+let last_name = "Augustine";
+let email = "augu0053@algonquinlive.com";
+
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+console.log(output);
